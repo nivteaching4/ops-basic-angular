30a160d960f023a22de4068fa654fef72676b803
nivteaching4 <128930216+nivteaching4@users.noreply.github.com>
2023-03-29 02:13:20 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
   url: 'https://ec2.nivitzhaky.com/api' 
 };
 
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
